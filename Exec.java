3fed19245f8a0d2b3fa5a46bf539d8b6be144d89
keid6 <hirkoi897@gmail.com>
2019-01-30 11:54:19 +0900

    ２回目のバージョン履歴

--- Exec.java
+++ Exec.java
@@ -1,5 +1,8 @@
 public class Exec{
 	public static void main(String[] args){
-	System.out.println("Git練習バージョン管理aaaadddd");
+	System.out.println("Git練習");
+	System.out.println("バージョン履歴");
+	System.out.println("3回目の履歴記録中");
+	System.out.println("テストテスト");
 	}
 }
